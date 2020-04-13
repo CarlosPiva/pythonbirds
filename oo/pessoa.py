7c88a8bb3c09e4ede9b2e851b212575f129146d7
Piva <carlos.r.piva@gmail.com>
2020-04-13 20:30:46 -0300

    Criado Atributos complexos filhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,16 +1,20 @@
 class Pessoa:
-    def __init__(self, nome=None, idade=35):
+    def __init__(self, *filhos, nome=None, idade=35):
         self.idade = idade
         self.nome = nome
+        self.filhos = list(filhos)
 
     def cumprimentar(self):
         return f"Olá {id(self)}"
+
+
 if __name__ == '__main__':
-    p = Pessoa("Carlos")
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar())
-    print(p.nome)
-    p.nome = 'Piva'
-    print(p.nome)
-    print(p.idade)
+    piva = Pessoa(nome='Piva')
+    carlos = Pessoa(piva, nome='Carlos')
+    print(Pessoa.cumprimentar(carlos))
+    print(id(carlos))
+    print(carlos.cumprimentar())
+    print(carlos.nome)
+    print(carlos.idade)
+    for filho in carlos.filhos:
+         print(filho.nome)
